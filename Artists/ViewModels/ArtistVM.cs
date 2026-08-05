7ab0e72296afa72421d8ecd711db9bc85eeab052
Nem207 <huynem07vn@gmail.com>
2026-08-05 12:40:05 +0700

    Delete Artists directory

--- Artists/ViewModels/ArtistVM.cs
+++
@@ -1,7 +0,0 @@
-namespace SpotifyClone.Features.Artist.ViewModels;
-public class ArtistVM
-{
-    public int ArtistID { get; set; }
-    public string ArtistName { get; set; } = "";
-    public string Avatar { get; set; } = "";
-}
