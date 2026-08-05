b486868d1cb64d2ae5e7786b499319a6a8f940d1
Nem207 <huynem07vn@gmail.com>
2026-08-05 12:43:26 +0700

    Delete AlbumArtist.cs

--- AlbumArtist.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpotifyClone.Models;
-public class AlbumArtist
-{
-    public int AlbumID { get; set; }
-    public int ArtistID { get; set; }
-    public Album Album { get; set; } = null!;
-    public Artist Artist { get; set; } = null!;
-}
