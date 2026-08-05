10c0c86b44d8f76386c943f0c1e0a804737ce3c2
Nem207 <huynem07vn@gmail.com>
2026-08-05 12:40:31 +0700

    Delete Albums directory

--- Albums/ViewModels/AblumVM.cs
+++
@@ -1,7 +0,0 @@
-namespace SpotifyClone.Features.Album.ViewModels;
-public class AlbumVM
-{
-    public int AlbumID { get; set; }
-    public string AlbumName { get; set; } = "";
-    public string CoverImage { get; set; } = "";
-}
