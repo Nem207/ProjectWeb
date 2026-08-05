f2dfd7cb5e7b52d4473fff5d087a7345ff1fcf2c
Nem207 <huynem07vn@gmail.com>
2026-08-05 15:40:36 +0700

    Add files via upload

---
+++ Source Code/view/ArtistDashboard/UploadSong.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace SpotifyClone.Views.ArtistDashboard
+{
+    public class UploadSongModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
