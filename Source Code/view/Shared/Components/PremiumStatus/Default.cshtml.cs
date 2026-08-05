bb2206ab4c9d212f51ca59f08439876d8245411a
Nem207 <huynem07vn@gmail.com>
2026-08-05 15:41:31 +0700

    Add files via upload

---
+++ Source Code/view/Shared/Components/PremiumStatus/Default.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace SpotifyClone.Views.Shared.Components.PremiumStatus
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Source Code/view/Shared/_Header.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace SpotifyClone.Views.Shared
+{
+    public class _HeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
