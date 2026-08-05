9464a3e3aeb2c95706e262de9b28701e33a78188
Nem207 <huynem07vn@gmail.com>
2026-08-05 12:43:43 +0700

    Delete PlaylistMember.cs

--- PlaylistMember.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpotifyClone.Models;
-public class PlaylistMember
-{
-    public int PlaylistID { get; set; }
-    public int UserID { get; set; }
-    public string? RoleName { get; set; }
-    public Playlist Playlist { get; set; } = null!;
-    public User User { get; set; } = null!;
-}
