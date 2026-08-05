04aa8af9223161590757df99c109796d70479b31
Nem207 <huynem07vn@gmail.com>
2026-08-05 12:39:22 +0700

    Delete Playlists directory

--- Playlists/ViewModels/PlaylistDetailVM.cs
+++
@@ -1,7 +0,0 @@
-using SpotifyClone.Features.Song.ViewModels;
-namespace SpotifyClone.Features.Playlist.ViewModels;
-public class PlaylistDetailVM
-{
-    public PlaylistVM Playlist { get; set; } = new();
-    public List<SongVM> Songs { get; set; } = new();
-}
--- Playlists/ViewModels/PlaylistSongOrderVM.cs
+++
@@ -1,5 +0,0 @@
-namespace SpotifyClone.Features.Playlist.ViewModels;
-public class PlaylistSongOrderVM
-{
-    public List<int> SongIDs { get; set; } = new();
-}
